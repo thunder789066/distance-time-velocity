180fe76ffa30f2bce7df49649ef211cbd61618ba
Christina Gerstner <christinalgerstner@gmail.com>
2020-04-07 19:14:29 -0500

    Initial commit

---
+++ Lab01/Lab01.cpp
@@ -0,0 +1,28 @@
+// Christina Gerstner
+// clgdtf@mail.umkc.edu
+// 8/23/2019
+// CS201L Lab 01
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	float distance, time, velocity;
+
+	cout << "Enter how many miles you are traveling " << endl;
+	cin >> distance;
+
+	cout << "How fast are you traveling in mph? " << endl;
+	cin >> velocity;
+
+    // Calculate the time it takes here.
+	time = distance/velocity;
+
+	cout << endl;
+
+	cout << "At " << velocity << " mph it will take " << time << " hours to travel " << distance << " miles." << endl;
+	
+	return 0;
+
+}
